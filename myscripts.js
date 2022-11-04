9f9dbd9f703a6637fda17e60ee7efe5b5170c21a
Tyler Randall <tyler7r@gmail.com>
2022-11-04 15:36:22 -0400

    Edit getComputerChoice function

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,10 @@
 function getComputerChoice () {
     let choice = Math.random () * 3;
     if (choice <=1) {
-        choice = "Rock";
+        return "Rock";
     } else if (choice <=2) {
-        choice = "Paper";
+        return "Paper";
     } else {
-        choice = "Scissors";
+        return "Scissors";
     }
 }
