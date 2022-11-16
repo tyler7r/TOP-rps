6da807c23c0b5781b1ef21439a06f3451aca53ff
Tyler Randall <tyler7r@gmail.com>
2022-11-16 15:18:23 -0500

    Add strict equality rules to playRound function

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,4 @@
-function playRound(playerSelection, computerSelection) 
-{ 
-    function getComputerChoice () {
+function getComputerChoice () {
     let choice = Math.random () * 3;
     if (choice <=1) {
         return "ROCK";
@@ -11,31 +9,34 @@ function playRound(playerSelection, computerSelection)
     }
 }
 
-    if ((computerSelection = 'ROCK') && (playerSelection = 'PAPER')) {
+let computerSelection = getComputerChoice();
+let playerSelection = prompt('Choose your hero: Rock, Paper, Scissors!').toUpperCase();
+
+function playRound(playerSelection, computerSelection) 
+{
+    if ((computerSelection === 'ROCK') && (playerSelection === 'PAPER')) {
         return "Player Wins, PAPER beats ROCK"; 
-    } else if ((computerSelection = 'ROCK') && (playerSelection = 'SCISSORS')) {
+    } else if ((computerSelection === 'ROCK') && (playerSelection === 'SCISSORS')) {
         return "Computer Wins, ROCK beats SCISSORS"
-    } else if ((computerSelection = 'ROCK') && (playerSelection = 'ROCK')) {
+    } else if ((computerSelection === 'ROCK') && (playerSelection === 'ROCK')) {
         return "TIE, ROCK vs. ROCK"
-    } else if ((computerSelection = 'PAPER') && (playerSelection = 'PAPER')) {
+    } else if ((computerSelection === 'PAPER') && (playerSelection === 'PAPER')) {
         return "TIE, PAPER vs. PAPER"
-    } else if ((computerSelection = 'PAPER') && (playerSelection = 'SCISSORS')) {
+    } else if ((computerSelection === 'PAPER') && (playerSelection === 'SCISSORS')) {
         return "Player Wins, SCISSORS beats PAPER"
-    } else if ((computerSelection = 'PAPER') && (playerSelection = 'ROCK')) {
+    } else if ((computerSelection === 'PAPER') && (playerSelection === 'ROCK')) {
         return "Computer Wins, PAPER beats ROCK"
-    } else if ((computerSelection = 'SCISSORS') && (playerSelection = 'PAPER')) {
+    } else if ((computerSelection === 'SCISSORS') && (playerSelection === 'PAPER')) {
         return "Computer Wins, SCISSORS beats PAPER"
-    } else if ((computerSelection = 'SCISSORS') && (playerSelection = 'SCISSORS')) {
+    } else if ((computerSelection === 'SCISSORS') && (playerSelection === 'SCISSORS')) {
         return "TIE, SCISSORS vs. SCISSORS"
-    } else if ((computerSelection = 'SCISSORS') && (playerSelection = 'ROCK')) {
+    } else if ((computerSelection === 'SCISSORS') && (playerSelection === 'ROCK')) {
         return "Player Wins, ROCK beats SCISSORS"
     } else {
         return "Oops, something went terribly wrong try again."
     }
     }
 
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt('Choose your hero: Rock, Paper, Scissors!').toUpperCase();
     console.log(playRound(playerSelection, computerSelection));
 
 
